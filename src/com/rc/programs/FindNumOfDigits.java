6781d4f163fd6a7f401f5c9e390ee7a555f82bec
Harshith Kumar M <harshithkumarm6@gmail.com>
2025-10-19 23:58:03 +0530

    number of digits program

---
+++ src/com/rc/programs/FindNumOfDigits.java
@@ -0,0 +1,19 @@
+package com.rc.programs;
+
+import java.util.Scanner;
+
+public class FindNumOfDigits {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the number");
+		int n = sc.nextInt();
+		int count=0;
+		while(n!=0)
+		{
+			n/=10;
+			count++;
+		}
+		System.out.println("number of digits present is "+count);
+	}
+
+}
