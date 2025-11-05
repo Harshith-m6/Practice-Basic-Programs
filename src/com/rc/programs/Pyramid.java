f75b9e0262128bff0f0444cee6fc77c76c94f46e
Harshith Kumar M <harshithkumarm6@gmail.com>
2025-11-05 12:19:24 +0530

    Patterns

---
+++ src/com/rc/programs/Pyramid.java
@@ -0,0 +1,25 @@
+package com.rc.programs;
+
+public class Pyramid{
+	
+	public static void main(String[] args) {
+		int row = 5;
+		for(int i = 1 ; i<= row ; i++)
+		{
+			for(int j = i ; j <row ; j++)
+			{
+				System.out.print("  ");
+			}
+			for(int j=1 ; j<=i ; j++ )
+			{
+				System.out.print("* ");
+			}
+			for(int j = 1 ; j<i ; j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
+}
---
+++ src/com/rc/programs/RightAlignedTriangle.java
@@ -0,0 +1,21 @@
+package com.rc.programs;
+
+public class RightAlignedTriangle {
+	public static void main(String[] args) {
+		
+		int rows = 5;
+		for(int i = 1 ; i <= rows ; i++)
+		{
+			for(int j = i ; j < rows  ; j++)
+			{
+				System.out.print("  ");
+			}
+			for(int j = 1 ; j <= i ; j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
+}
