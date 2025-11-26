8f4a2cd7662d01fb3d3b6c974bd933eedee3c878
Harshith Kumar M <harshithkumarm6@gmail.com>
2025-11-26 11:15:55 +0530

    MultiThreading

---
+++ src/com/rc/programs/MultiThreadingThread1.java
@@ -0,0 +1,7 @@
+package com.rc.programs;
+
+public class MultiThreadingThread1 {
+	//if we want to execute multiple task simultaneously we want to load them in different stack
+	
+
+}
--- src/com/rc/programs/RecursionReverseArray.java
+++ src/com/rc/programs/RecursionReverseArray.java
@@ -1,5 +1,28 @@
 package com.rc.programs;
 
+import java.util.Arrays;
+
 public class RecursionReverseArray {
+	
+	static void reverse(int[] x , int start , int end)
+	{
+		if(start>=end)
+			return;
+		int temp=x[start];
+		x[start]=x[end];
+		x[end]=temp;
+		reverse(x , start+1 , end-1);
+	}
+	
+	static void reverse(int[] x)
+	{
+		reverse(x,0,x.length-1);
+	}
+	
+	public static void main(String[] args) {
+		int [] ar = {10,20,30,40,50,60};
+		reverse(ar);
+		System.out.println("Reversed array "+Arrays.toString(ar));
+	}
 
 }
