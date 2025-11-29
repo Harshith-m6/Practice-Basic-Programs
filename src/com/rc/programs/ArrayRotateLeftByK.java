5632452d603444fe068f82a665d4d2d0bb70a6aa
Harshith Kumar M <harshithkumarm6@gmail.com>
2025-11-29 23:55:54 +0530

    Programs on Array

---
+++ src/com/rc/programs/ArrayRotateLeftByK.java
@@ -0,0 +1,34 @@
+package com.rc.programs;
+
+import java.util.Scanner;
+
+public class ArrayRotateLeftByK {
+	public static void main(String[] args) {
+		int[] a = {1,2,3,4,5,6,7,8,9};
+		System.out.println("enter the order of rotation");
+		Scanner sc = new Scanner(System.in);
+		int k = sc.nextInt();
+		
+		rev(a,0,k-1);
+		rev(a,k,a.length-1);
+		rev(a,0,a.length-1);
+		
+		for(int x : a)
+		{
+			System.out.print(x+"\s");
+		}
+	}
+	
+	static void rev(int[] a , int start, int end )
+	{
+		while(start<end)
+		{
+			int temp = a[start];
+			a[start]=a[end];
+			a[end]=temp;
+			start++;
+			end--;
+		}
+	}
+
+}
---
+++ src/com/rc/programs/ArrayRotateRightByK.java
@@ -0,0 +1,39 @@
+package com.rc.programs;
+
+import java.util.Scanner;
+
+public class ArrayRotateRightByK {
+	public static void main(String[] args) {
+		int[] a = {0,1,2,3,4,5,6,7,8,9,10};
+		System.out.println("Enter the order by which element should rotate");
+		Scanner sc = new Scanner(System.in);
+		int k = sc.nextInt();
+		
+		k =k %10;
+		
+		rev(a,0,a.length-1);
+		rev(a,0,k-1);
+		rev(a,k,a.length-1);
+		
+		for(int x : a)
+		{
+			System.out.print(x+"\s");
+		}
+		
+		
+		
+	}
+	
+	static void rev(int[] a , int first , int last)
+	{
+		while(first<last)
+		{
+			int temp = a[first];
+			a[first] = a[last];
+			a[last] = temp;
+			first++;
+			last--;
+		}
+	}
+
+}
