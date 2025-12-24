6b3403469e437edb03da792cf948a27d7476f933
Harshith Kumar M <harshithkumarm6@gmail.com>
2025-12-24 23:17:25 +0530

    Number system programs

---
+++ src/com/rc/programs/Practice2.java
@@ -0,0 +1,18 @@
+package com.rc.programs;
+
+public class Practice2 {
+	public static void main(String[] args) {
+		int a =40;
+		int b =20;
+		int c =30;
+		
+		int max = a;
+		
+		if(b>max) max=b;
+		if(c>max) max=c;
+		System.out.println(max+" is greater");
+		
+		
+	}
+
+}
---
+++ src/com/rc/programs/Practice3.java
@@ -0,0 +1,18 @@
+package com.rc.programs;
+
+public class Practice3 {
+	public static void main(String[] args) {
+		
+		int a = 0;
+		if(a>0)
+		{
+			System.out.println("Positive");
+		}else if(a<0)
+		{
+			System.out.println("Negetive");
+		}else {
+			System.out.println("Zero");
+		}
+	}
+
+}
