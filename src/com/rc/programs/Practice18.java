69a9fcd2c55bf9ffea5f75527c875751ff2be9c1
Harshith Kumar M <harshithkumarm6@gmail.com>
2025-12-25 18:49:08 +0530

    Pattern Programs

---
+++ src/com/rc/programs/Practice18.java
@@ -0,0 +1,16 @@
+package com.rc.programs;
+
+public class Practice18 {
+	public static void main(String[] args) {
+		int n = 5;
+		for(int i = n ; i >= 1 ; i--)
+		{
+			for(int k = n ; k>=1 ; k--)
+			{
+				System.out.print( " * ");
+			}
+			System.out.println();
+		}
+	}
+
+}
